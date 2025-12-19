fc9d05e340506ad766146d9f2f86db5ca2ab1cd9
Valentin Weyer <valentin.weyer@gmail.com>
2025-12-19 23:59:37 +0100

    First Tests of Handball court config

--- sports/basketball/__init__.py
+++ sports/basketball/__init__.py
@@ -1,16 +1,13 @@
 from sports.basketball.annotators import draw_court, draw_made_and_miss_on_court, \
     draw_points_on_court, draw_paths_on_court
-from sports.basketball.config import CourtConfiguration, League
 from sports.basketball.tools import ShotType, ShotEvent, ShotEventTracker
+
+from sports.handball.config import CourtConfiguration, League  # League = your handball enum (e.g., IHF)
 
 __all__ = [
     "CourtConfiguration",
     "League",
     "draw_court",
-    "draw_made_and_miss_on_court",
     "draw_points_on_court",
     "draw_paths_on_court",
-    "ShotType",
-    "ShotEvent",
-    "ShotEventTracker"
 ]
